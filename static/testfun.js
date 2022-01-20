665e835e0a017bf5d67acee158cd917f68333f6c
gc-westover <47981240+gc-westover@users.noreply.github.com>
2022-01-20 14:55:30 -0500

    Update testfun.js

--- static/testfun.js
+++ static/testfun.js
@@ -1,7 +1,9 @@
 function testfun(){
+
+const appUrl = 'https://prod-05.northcentralus.logic.azure.com:443/workflows/897e0dba354d42c98a016e9ec56b5b7e/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nOxZnDGzdiP6DDcp6nVGkrrXt6rtb_eaQiAVpyJHH5Q';
 const date = new Date(Date.now());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let x = axios.post('REPLACE WITH URL FOR LOGIC APP', {
+let x = axios.post(appUrl, {
     id: self.crypto.randomUUID(),
     month: months[date.getMonth()],
     day: date.getDate(),
