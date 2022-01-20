6ce1ff14b5dbd3ac6bf36f9b8a8c1b30ff86fb60
gc-westover <47981240+gc-westover@users.noreply.github.com>
2022-01-20 16:21:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     
-#     try:
-#         db = client.get_database_client(DATABASE_ID)
-#         container = db.get_container_client(CONTAINER_ID)
-#         data = container.query_items(query='SELECT * FROM container', enable_cross_partition_query=True,)
-#     except exceptions.CosmosHttpResponseError as e:
-#         print('\nrun_sample has caught an error. {0}'.format(e.message))
+    try:
+        db = client.get_database_client(DATABASE_ID)
+        container = db.get_container_client(CONTAINER_ID)
+        data = container.query_items(query='SELECT * FROM container', enable_cross_partition_query=True,)
+    except exceptions.CosmosHttpResponseError as e:
+        print('\nrun_sample has caught an error. {0}'.format(e.message))
 
-#     finally:
-#         print("\nrun_sample done")
+    finally:
+        print("\nrun_sample done")
     data = {}
     return render_template('testweb.html', data=data)
 
