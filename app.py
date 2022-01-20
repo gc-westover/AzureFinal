a38e5f64a361e101a058d56d9e19f629a9bc51ba
Greg W <gcwestover@gmail.com>
2022-01-20 17:29:28 -0500

    commit

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ MASTER_KEY = os.environ.get('ACCOUNT_KEY'),
 DATABASE_ID = os.environ.get('COSMOS_DATABASE'),
 CONTAINER_ID = os.environ.get('COSMOS_CONTAINER'),
 
-client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     
     try:
+        client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
         db = client.get_database_client(DATABASE_ID)
         container = db.get_container_client(CONTAINER_ID)
         data = container.query_items(query='SELECT * FROM container ORDER BY container.second DESC', enable_cross_partition_query=True,)
