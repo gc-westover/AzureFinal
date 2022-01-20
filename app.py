bed16fbdc0132fc845412713ec3c895c5bbe6fda
Greg W <gcwestover@gmail.com>
2022-01-20 17:13:30 -0500

    commit

--- app.py
+++ app.py
@@ -19,15 +19,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     
-    try:
-        db = client.get_database_client(DATABASE_ID)
-        container = db.get_container_client(CONTAINER_ID)
-        data = container.query_items(query='SELECT * FROM container ORDER BY container.second DESC', enable_cross_partition_query=True,)
-    except exceptions.CosmosHttpResponseError as e:
-        print('\nrun_sample has caught an error. {0}'.format(e.message))
-    finally:
-        print("\nrun_sample done")
-
+    # try:
+    #     db = client.get_database_client(DATABASE_ID)
+    #     container = db.get_container_client(CONTAINER_ID)
+    #     data = container.query_items(query='SELECT * FROM container ORDER BY container.second DESC', enable_cross_partition_query=True,)
+    # except exceptions.CosmosHttpResponseError as e:
+    #     print('\nrun_sample has caught an error. {0}'.format(e.message))
+    # finally:
+    #     print("\nrun_sample done")
+    data = []
     return render_template('testweb.html', data=data, settings=[HOST, MASTER_KEY, DATABASE_ID, CONTAINER_ID])
 
 if __name__ == '__main__':
